ca76cfd0583c984d1a665be1c8170a4e02751d3b
JsemOlik <github@jsemolik.dev>
2026-04-25 17:36:26 +0200

    Add project files.

---
+++ Cookie-Engine/Cookie-Engine.cpp
@@ -0,0 +1,12 @@
+﻿// Cookie-Engine.cpp : Defines the entry point for the application.
+//
+
+#include "Cookie-Engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
